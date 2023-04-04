3700d8ceaa5bd6e48e1033bf716cd45aa7743190
poo0054 <poo0054@outlook.com>
2023-04-04 17:35:57 +0800

    修复校验注解作用率 注释

--- dian1-spring-boot-autoconfigure/http-spring-boot-autoconfigure/src/main/java/com/dian1/http/annotate/parameter/HttpValidated.java
+++ dian1-spring-boot-autoconfigure/http-spring-boot-autoconfigure/src/main/java/com/dian1/http/annotate/parameter/HttpValidated.java
@@ -6,12 +6,12 @@ import javax.validation.groups.Default;
 import java.lang.annotation.*;
 
 /**
- * 在方法上所有参数都会校验,在参数上,校验当前参数
+ * 在参数上,校验当前参数
  *
  * @author zhangzhi
  * @date 2023/4/4
  */
-@Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface HttpValidated {
